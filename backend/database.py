a48be7a0640004929cd2d3dd37556ba7c7f5cffa
Utkarsh Agarwal <42709439+agarwalutkarsh@users.noreply.github.com>
2026-07-12 11:08:18 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-DATABASE_URL = "postgresql://postgres:Pl%40net%404757@localhost:5432/badminton_analysis"
+from config import settings
+
+DATABASE_URL = settings.DATABASE_URL
 
 engine = create_engine(DATABASE_URL)
 
